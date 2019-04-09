bb1e4cd4f0684e581c128f4511a4d70fc3ae6c2d
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-04-09 02:00:12 -0500

    Add files via upload

--- webmap2.js
+++ webmap2.js
@@ -1,5 +1,5 @@
 let myMap = L.map('map2').setView([32.18, -99.14], 4)
-let basemapUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+let basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 L.tileLayer(basemapUrl).addTo(myMap)
 L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
