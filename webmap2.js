f2b10d5a60fedc83e46340df52b74d662f08da63
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-04-09 01:40:03 -0500

    Add files via upload

--- webmap2.js
+++ webmap2.js
@@ -7,4 +7,9 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(myMap)
-
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(myMap)
