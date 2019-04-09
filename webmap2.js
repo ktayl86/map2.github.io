0847086c0474cae541019ac5a674e76de401855f
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-04-09 01:36:11 -0500

    Update webmap2.js

--- webmap2.js
+++ webmap2.js
@@ -7,9 +7,4 @@ L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(myMap)
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA'
-}).addTo(myMap)
+
