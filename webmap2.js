46698bc92904cb9fd6e9f4e0d523200761b9b7a8
ktayl86 <48958922+ktayl86@users.noreply.github.com>
2019-04-09 01:33:50 -0500

    Add files via upload

---
+++ webmap2.js
@@ -0,0 +1,15 @@
+let myMap = L.map('map2').setView([32.18, -99.14], 4)
+let basemapUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+L.tileLayer(basemapUrl).addTo(myMap)
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(myMap)
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
+}).addTo(myMap)
